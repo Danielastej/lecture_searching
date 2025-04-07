d778e66b2dc353082032dc4392de7f55595743ad
Danielastej <272800@vutbr.cz>
2025-04-07 13:33:59 +0200

    searched

--- searching.py
+++ searching.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+from setuptools.dist import sequence
+
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -21,12 +23,24 @@ def read_data(file_name, field):
         print(f'field {field} not exist.')
         return None
 
+def linear_search(list_of_numbers, number):
+    list_of_idxs = []
+    for idx, element in enumerate(list_of_numbers):
+        if element == number:
+            list_of_idxs.append(idx)
+        else:
+            pass
+    return {"positions": list_of_idxs, "count": len(list_of_idxs)}
+
+
 def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
     json_filename = "sequential.json"
     my_data = read_data(json_filename, "unordered_numbers")
     print(my_data)
+    found_numbers_linear = linear_search(my_data,0)
+
+if __name__ == '__main__':
+    my_list = [1, 2, 5, 7]
+    searched_number = 5
+    found_numbers = linear_search(my_list, searched_number)
+    print(found_numbers)
