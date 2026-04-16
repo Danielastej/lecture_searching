9e45f1649e35054f5dfdcbe822a3bc0d5b309ab3
Danielastej <272800@vutbr.cz>
2026-04-16 15:15:30 +0200

    linearni hledani

--- searching.py
+++ searching.py
@@ -15,9 +15,24 @@ def read_data(file_name, field):
         return None
 
 
+def linear_search(sequence, searched_number = 2):
+    positions = []
+    count = 0
+    i = 0
+    for number in sequence:
+        if number == searched_number:
+            count += 1
+            positions.append(i)
+        i += 1
+    return positions, count
+
+
+
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
+    searching_number = linear_search([2,4,5,8,30,26,5,4,5,5,4,8], 5)
+    print(searching_number)
 
 
 if __name__ == "__main__":
